6de3df53f7d28af3f7363e7d822c981809bee075
Amlac Eniotna <antoine.calma30@gmail.com>
2024-04-08 13:28:51 +0200

    fichier de typage

---
+++ frontend/src/type.ts
@@ -0,0 +1,12 @@
+export type userData = {
+  email: string
+  firstName: string
+  lastName: string
+  userName: string
+  token: string
+}
+
+export type action = {
+  type: string
+  payload?: string
+}
