3cec83216df72baf9e2db1d43e5cc79c1e72b055
Amlac Eniotna <antoine.calma30@gmail.com>
2024-04-08 13:32:19 +0200

    fetch login + sauvegarde du token dans le store

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,6 +1,36 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [click, setClick] = useState(false)
+  const [btn, setBtn] = useState(false)
+  const [remember, setRemember] = useState(false)
+  const dispatch = useDispatch()
+
+  const formValue = {
+    email: username,
+    password: password,
+  }
+  useEffect(() => {
+    async function asyncer() {
+      if (click) {
+        const data = await fetchData(formValue)
+        if (data.status === 200) {
+          dispatch({ type: 'login', payload: data.body.token })
+          if (remember) {
+            localStorage.setItem('token', data.body.token)
+          } else {
+            localStorage.removeItem('token')
+          }
+        }
+      }
+    }
+    asyncer()
+  }, [btn])
+
   return (
     <>
       <main className="main bg-dark">
@@ -10,19 +40,38 @@ function Login() {
           <form>
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <input
+                type="text"
+                id="username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="input-remember">
-              <input type="checkbox" id="remember-me" />
+              <input
+                type="checkbox"
+                id="remember-me"
+                onChange={(e) => setRemember(e.target.checked)}
+              />
               <label htmlFor="remember-me">Remember me</label>
             </div>
-            <Link to="/user">
-              <button className="sign-in-button">Sign In</button>
-            </Link>
+            <button
+              className="sign-in-button"
+              onClick={(e) => {
+                e.preventDefault()
+                setClick(true)
+                setBtn(!btn)
+              }}
+            >
+              Sign In
+            </button>
           </form>
         </section>
       </main>
@@ -31,3 +80,19 @@ function Login() {
 }
 
 export default Login
+
+async function fetchData(formValue: { email: string; password: string }) {
+  try {
+    const res = await fetch('http://localhost:3001/api/v1/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formValue),
+    })
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
