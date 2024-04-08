64afda563618077ff6d69e2f17256d35fc697fae
Amlac Eniotna <antoine.calma30@gmail.com>
2024-04-08 13:32:30 +0200

    linter rbnb

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -10,6 +12,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'no-console': 2,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
